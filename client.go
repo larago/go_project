3039421dd3937c36662b4e1622504b520b0550d2
larago <bingeye@foxmail.com>
2017-01-13 23:34:31 +0800

    Initilaize git repos

---
+++ client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func sender(conn net.Conn) {
+	words := "hello world!"
+	conn.Write([]byte(words))
+	fmt.Println("send over")
+}
+
+func main() {
+	server := "127.0.0.1:1025"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal Error: %s", err.Error())
+	}
+
+	fmt.Println("connect success")
+	sender(conn)
+}
